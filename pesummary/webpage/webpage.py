565850b987ad60b1e9ca9ea1f57a549190324778
Charlie Hoy <hoyc1@cardiff.ac.uk>
2019-02-06 09:21:23 +0000

    adding Ed as contributor to webpage code

--- pesummary/webpage/webpage.py
+++ pesummary/webpage/webpage.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2018  Charlie Hoy <charlie.hoy@ligo.org>
+#                     Edward Fauchon-Jones <edward.fauchon-jones@ligo.org>
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
 # Free Software Foundation; either version 3 of the License, or (at your
