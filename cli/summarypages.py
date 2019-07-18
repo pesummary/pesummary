b323bed33d9bde57777d14aad0fd72f3babb87bb
Charlie Hoy <charlie.hoy@ligo.org>
2019-07-18 15:21:28 -0500

    Calibration and psd plots not showing up on homepage

--- cli/summarypages.py
+++ cli/summarypages.py
@@ -664,15 +664,15 @@ class GWWebpageGeneration(pesummary.gw.inputs.GWPostProcessing, WebpageGeneratio
                 images = [y for x in image_contents for y in x]
                 html_file.make_modal_carousel(images=images)
         else:
-            if os.path.isfile(path + "%s_strain.png" % (self.labels[0])):
+            if os.path.isfile(self.webdir + "/plots/%s_strain.png" % (self.labels[0])):
                 image_contents.append(path + "%s_strain.png" % (self.labels[0]))
-            if os.path.isfile(path + "%s_waveform_timedoain.png" % (self.labels[0])):
+            if os.path.isfile(self.webdir + "/plots/%s_waveform_timedomain.png" % (self.labels[0])):
                 image_contents.append(path + "%s_waveform_timedomain.png" % (
                     self.labels[0]))
-            if os.path.isfile(path + "%s_psd_plot.png" % (self.labels[0])):
+            if os.path.isfile(self.webdir + "/plots/%s_psd_plot.png" % (self.labels[0])):
                 image_contents.append(path + "%s_psd_plot.png" % (
                     self.labels[0]))
-            if os.path.isfile(path + "%s_calibration_plot.png" % (self.labels[0])):
+            if os.path.isfile(self.webdir + "/plots/%s_calibration_plot.png" % (self.labels[0])):
                 image_contents.append(path + "%s_calibration_plot.png" % (
                     self.labels[0]))
             image_contents = [image_contents]
