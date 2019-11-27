cd931185a2710ba4b264c7f6bebb47b416244674
Charlie Hoy <hoyc1@cardiff.ac.uk>
2019-11-27 00:06:30 +0000

    typo

--- pesummary/gw/plots/publication.py
+++ pesummary/gw/plots/publication.py
@@ -20,7 +20,8 @@ import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 import seaborn
 from pesummary.gw.plots import violin
-from pesummary.gw.plots.bounded_2d_kde import Bounded_2d_kde, default_bounds
+from pesummary.gw.plots.bounded_2d_kde import Bounded_2d_kde
+from pesummary.gw.plots.bounds import default_bounds
 from pesummary.gw.plots.cmap import colormap_with_fixed_hue
 from pesummary.gw.file.conversions import mchirp_from_m1_m2, q_from_m1_m2
 import numpy as np
