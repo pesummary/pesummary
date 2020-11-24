4c86102aed00a1cb00bb284378b1e8f636dab87c
Aditya Vijaykumar <vijaykumar.aditya@gmail.com>
2020-11-24 18:06:29 +0530

    added first version of the function: more changes are needed

--- pesummary/gw/file/tgr.py
+++ pesummary/gw/file/tgr.py
@@ -14,11 +14,93 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from pesummary.core.plots.bounded_2d_kde import Bounded_2d_kde
+import numpy as np
+
+
+def P_integrand(chif, Mf, v1, v2, P_Mfchif_i_interp_object, P_Mfchif_r_interp_object):
+
+    """Compute the integrand of P(dMf/Mfbar, dchif/chifbar).
+
+    inputs:
+    chif: vector of values of final spin
+    Mf: vector of values of final mass
+    v1: dMf/Mfbar value
+    v2: dchif/chifbar value
+    P_Mfchif_i_interp_object: interpolation function of P_i(Mf, chif)
+    P_Mfchif_r_interp_object: interpolation function of P_r(Mf, chif)
+
+    output: integrand of P(dMf/Mfbar, dchif/chifbar)
+    """
+
+    Mf_mat, chif_mat = np.meshgrid(Mf, chif)
+
+    # Create dMf and dchif vectors corresponding to the given v1 and v2. These vectors have to be
+    # monotonically increasing in order to evaluate the interpolated prob densities. Hence, for
+    # v1, v2 < 0, flip them, evaluate the prob density (in column or row) and flip it back
+    dMf_i = (1.0 + v1 / 2.0) * Mf
+    dchif_i = (1.0 + v2 / 2.0) * chif
+
+    dMf_r = (1.0 - v1 / 2.0) * Mf
+    dchif_r = (1.0 - v2 / 2.0) * chif
+
+    if (1.0 + v1 / 2.0) < 0.0:
+        dMf_i = np.flipud(dMf_i)
+    if (1.0 + v2 / 2.0) < 0.0:
+        dchif_i = np.flipud(dchif_i)
+    P_i = P_Mfchif_i_interp_object([dMf_i, dchif_i])
+
+    if (1.0 + v1 / 2.0) < 0.0:
+        P_i = np.fliplr(P_i)
+    if (1.0 + v2 / 2.0) < 0.0:
+        P_i = np.flipud(P_i)
+
+    if (1.0 - v1 / 2.0) < 0.0:
+        dMf_r = np.flipud(dMf_r)
+    if (1.0 - v2 / 2.0) < 0.0:
+        dchif_r = np.flipud(dchif_r)
+    P_r = P_Mfchif_r_interp_object([dMf_r, dchif_r])
+
+    if (1.0 - v1 / 2.0) < 0.0:
+        P_r = np.fliplr(P_r)
+    if (1.0 - v2 / 2.0) < 0.0:
+        P_r = np.flipud(P_r)
+
+    return P_i * P_r * abs(Mf_mat * chif_mat), P_i, P_r
+
+
+def calc_sum(Mf, chif, v1, v2, P_Mfchif_i_interp_object, P_Mfchif_r_interp_object):
+    Pintg, P_i, P_r = P_integrand(chif, Mf, v1, v2, P_Mfchif_i_interp_object, P_Mfchif_r_interp_object)
+    return np.sum(Pintg)
 
 
 def imrct_delta_parameters_from_Mf_af(
-    Mf_inspiral, af_inspiral, Mf_postinspiral, af_postinspiral, Mf_lim=[-2, 2], af_lim=[-1, 1]
+    Mf_inspiral, chif_inspiral, Mf_postinspiral, chif_postinspiral, dMfbyMf_lim=2, dchifbychif_lim=1, N_bins=401
 ):
-    kde_kwargs = dict(xlow=Mf_lim[0], xhigh=Mf_lim[0], ylow=Mf_lim[0], yhigh=af_lim[0])
-    inspiral_kde = Bounded_2d_kde([Mf_inspiral, af_inspiral], **kde_kwargs)
-    postinspiral_kde = Bounded_2d_kde([Mf_postinspiral, af_postinspiral], **kde_kwargs)
+    Mf_lim = np.amax(np.append(Mf_inspiral, Mf_postinspiral))
+    Mf_bins = np.linspace(-Mf_lim, Mf_lim, N_bins)
+    chif_lim = np.amax(np.append(chif_inspiral, chif_postinspiral))
+    chif_bins = np.linspace(-chif_lim, chif_lim, N_bins)
+    Mf_intp = (Mf_bins[:-1] + Mf_bins[1:]) / 2
+    chif_intp = (chif_bins[:-1] + chif_bins[1:]) / 2.0
+
+    inspiral_kde = Bounded_2d_kde(np.array([Mf_inspiral, chif_inspiral]))
+    postinspiral_kde = Bounded_2d_kde(np.array([Mf_postinspiral, chif_postinspiral]))
+
+    dMfbyMf_vec = np.linspace(-dMfbyMf_lim, dMfbyMf_lim, N_bins)
+    dchifbychif_vec = np.linspace(-dchifbychif_lim, dchifbychif_lim, N_bins)
+
+    diff_dMfbyMf = np.mean(np.diff(dMfbyMf_vec))
+    diff_dchifbychif = np.mean(np.diff(dchifbychif_vec))
+
+    P_dMfbyMf_dchifbychif = np.zeros(shape=(N_bins, N_bins))
+    for i, v2 in enumerate(dchifbychif_vec):
+        for j, v1 in enumerate(dMfbyMf_vec):
+            P_dMfbyMf_dchifbychif[i, j] = calc_sum(Mf_intp, chif_intp, v1, v2, inspiral_kde, postinspiral_kde)
+
+    P_dMfbyMf_dchifbychif /= np.sum(P_dMfbyMf_dchifbychif) * diff_dMfbyMf * diff_dchifbychif
+
+    # Marginalization to one-dimensional joint_posteriors
+
+    P_dMfbyMf = np.sum(P_dMfbyMf_dchifbychif, axis=0) * diff_dchifbychif
+    P_dchifbychif = np.sum(P_dMfbyMf_dchifbychif, axis=1) * diff_dMfbyMf
+    return P_dMfbyMf, P_dchifbychif, P_dMfbyMf_dchifbychif, dMfbyMf_vec, dchifbychif_vec
