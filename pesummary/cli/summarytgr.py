24d27ffd4c59e2a87df311d4103fcab42bd4749c
Aditya Vijaykumar <vijaykumar.aditya@gmail.com>
2020-12-12 13:44:01 +0530

    added Charlie's name to the LICENSE for summarytgr

--- pesummary/cli/summarytgr.py
+++ pesummary/cli/summarytgr.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 # Copyright (C) 2020  Aditya Vijaykumar <aditya.vijaykumar@ligo.org>
+#                     Charlie Hoy <charlie.hoy@ligo.org>
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
 # Free Software Foundation; either version 3 of the License, or (at your
