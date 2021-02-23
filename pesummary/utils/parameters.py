06e0d7d3a21a9a4d3b44320cdfee6f3724242029
Charlie Hoy <charlie.hoy@ligo.org>
2021-02-23 15:56:52 -0600

    Standard names on webpages

--- pesummary/utils/parameters.py
+++ pesummary/utils/parameters.py
@@ -59,9 +59,50 @@ class Parameters(List):
         list of parameters that have been appended to the original list
     """
     def __init__(self, *args, cls=Parameter, **kwargs):
+        self._cls = cls
         if len(args) == 1:
             super(Parameters, self).__init__(*args, cls=Parameter, **kwargs)
         else:
             _args = list(args)
             _args[2] = Parameter
             super(Parameters, self).__init__(*_args, **kwargs)
+
+    def __iter__(self, *args, **kwargs):
+        _iter = super(Parameters, self).__iter__(*args, **kwargs)
+        for _value in _iter:
+            yield self._cls(_value)
+
+
+class MultiAnalysisParameters(List):
+    """Class to store a multidimensional list of parameters
+
+    Parameters
+    ----------
+    args: tuple
+        all arguments are passed to the list class
+    **kwargs: dict
+        all kwargs are passed to the list class
+
+    Attributes
+    ----------
+    added: list
+        list of parameters that have been appended to the original list
+    """
+    def __init__(self, *args, cls=Parameters, **kwargs):
+        self._cls = cls
+        if len(args) == 1:
+            super(MultiAnalysisParameters, self).__init__(
+                *args, cls=cls, **kwargs
+            )
+        else:
+            _args = list(args)
+            _args[2] = Parameters
+            super(MultiAnalysisParameters, self).__init__(*_args, **kwargs)
+
+    def __getitem__(self, *args, **kwargs):
+        self.cls = None
+        output = super(List, self).__getitem__(*args, **kwargs)
+        self.cls = self._cls
+        if isinstance(output[0], list):
+            return [self.cls(value) for value in output]
+        return self.cls(output)
