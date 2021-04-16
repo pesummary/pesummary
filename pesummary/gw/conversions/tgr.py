adba319af8a1fd04511eacec54478dcdd40dc6c7
Charlie Hoy <hoyc1@cardiff.ac.uk>
2021-04-16 09:05:40 +0100

    mit license

--- pesummary/gw/conversions/tgr.py
+++ pesummary/gw/conversions/tgr.py
@@ -1,18 +1,4 @@
-# Copyright (C) 2020  Aditya Vijaykumar <aditya.vijaykumar@ligo.org>
-#                     Charlie Hoy <charlie.hoy@ligo.org>
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation; either version 3 of the License, or (at your
-# option) any later version
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
-# Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Licensed under an MIT style license -- see LICENSE.md
 
 from pesummary.core.plots.bounded_2d_kde import Bounded_2d_kde
 from pesummary.utils.utils import logger
@@ -21,6 +7,11 @@ from scipy.stats import gaussian_kde
 from scipy.interpolate import interp2d
 import multiprocessing
 from pesummary.utils.probability_dict import ProbabilityDict2D
+
+__author__ = [
+    "Aditya Vijaykumar <aditya.vijaykumar@ligo.org>",
+    "Charlie Hoy <charlie.hoy@ligo.org>"
+]
 
 
 def _wrapper_for_multiprocessing_kde(kde, *args):
